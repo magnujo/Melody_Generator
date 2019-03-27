c08d25ef0320e628035f69ecc18e296c559b5f00
w32siooo <creeco@gmail.com>
2019-03-27 15:16:26 +0100

    filereader opdateret

--- src/com/company/fileReader.java
+++ src/com/company/fileReader.java
@@ -8,69 +8,84 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class fileReader {
+    ArrayList<Integer> playlist = new ArrayList<>();
     int notePlayed=0;
     private String path;
+    String aline = null;
+
 
     private fileReader(String path) {
         this.path = path;
     }
+
+
     private ArrayList<Integer> playNote() {
 
-        ArrayList<Integer> playlist = new ArrayList<>();
-        playlist.add(5);
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader =
+                    new FileReader(path );
 
-        Scanner scanner = new Scanner(path);
-            while (scanner.hasNext()) {
-                String line = scanner.nextLine();
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while((aline = bufferedReader.readLine()) != null) {
 
                 Pattern ae = Pattern.compile("[a-e]");   // the pattern to search for
-                Matcher m = ae.matcher(line);
+                Matcher m = ae.matcher(aline);
 
                 Pattern hn = Pattern.compile("[f-j]");   // the pattern to search for
-                Matcher m2 = hn.matcher(line);
+                Matcher m2 = hn.matcher(aline);
 
                 Pattern ko = Pattern.compile("[k-o]");   // the pattern to search for
-                Matcher m3 = ko.matcher(line);
+                Matcher m3 = ko.matcher(aline);
 
                 Pattern pt = Pattern.compile("[p-t]");   // the pattern to search for
-                Matcher m4 = pt.matcher(line);
+                Matcher m4 = pt.matcher(aline);
 
                 Pattern uz = Pattern.compile("[u-z]");   // the pattern to search for
-                Matcher m5 = uz.matcher(line);
+                Matcher m5 = uz.matcher(aline);
 
                 if (m.find()) {
-                   // System.out.println("found 1");
+                    // System.out.println("found 1");
                     playlist.add(1);
                 }
                 if (m2.find()) {
-                   // System.out.println("found 2");
+                    // System.out.println("found 2");
                     playlist.add(2);
 
                 }
                 if (m3.find()) {
-                 //   System.out.println("found 3");
+                    //   System.out.println("found 3");
                     playlist.add(3);
 
                 }
                 if (m4.find()) {
-                   // System.out.println("found 4");
+                    // System.out.println("found 4");
                     playlist.add(4);
 
                 }
                 if (m5.find()) {
-                   // System.out.println("found 5");
+                    // System.out.println("found 5");
                     playlist.add(5);
                 }
             }
-                    return playlist;
-    }
+        } catch (IOException e) {
+            System.out.println("Something went wrong.");
+        }
+
+        return playlist;
+   }
 
     public static void main(String[] args) {
 
-        fileReader fR = new fileReader(".idea/data");
-        System.out.println(fR.playNote());
+
         //fR.playnote();
         //System.out.println(fR.notePlayed);
+        
+            fileReader fR = new fileReader(".idea/data");
+            System.out.println(fR.playNote());
 
     }
 }
