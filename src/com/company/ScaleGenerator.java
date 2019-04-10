b95ae0da091cf8a805d43353dcd3f0e463028226
w32siooo <47818670+w32siooo@users.noreply.github.com>
2019-04-10 12:48:28 +0200

    scalegenerator + majorscale

--- src/com/company/ScaleGenerator.java
+++ src/com/company/ScaleGenerator.java
@@ -1,22 +1,31 @@
 package com.company;
 
-public class ScaleGenerator {
-    public static final double perfectE = 440;
-    private static double[] scale = new double[7];
+import java.util.Arrays;
 
-    public ScaleGenerator(){
+public class ScaleGenerator {
+    //this method generates a chromatic scale from any starting frequency.
+    //A frequency table can be found on http://pages.mtu.edu/~suits/notefreqs.html
+
+    //This array of doubles stores the scale.
+    protected static double[] scale;
+
+    //This is the constructor which can take a length (there are 12 notes in a chromatic octave), and a starting frequency. I refer to the table mentioned above.
+    public ScaleGenerator(int scaleLength, double startFreq){
+
+        scale = new double[scaleLength];
+
         for (int i = 0; i < scale.length ; i++) {
             // The frequency needs to double every \{12}12 notes (because there are \{7}7 white notes and \{5}5 black notes in each octave.)
-            scale[i] = 440*Math.pow(2,i/12.0);
+            scale[i] = startFreq*Math.pow(2,i/12.0);
         }
 
     }
 
     public static void main(String[] args) {
 
-        ScaleGenerator scaleGenerator = new ScaleGenerator();
+        ScaleGenerator scaleGenerator = new ScaleGenerator(13,440);
 
-        System.out.println(scaleGenerator.scale[6]);
+        System.out.println(Arrays.toString(scale));
     }
 }
 
--- src/com/company/Skala.java
+++ src/com/company/Skala.java
@@ -27,9 +27,4 @@ public class Skala {
         this.cSkala = cSkala;
     }
 
-
-
-
-
-
 }
