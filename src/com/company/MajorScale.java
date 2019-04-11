1d869fd637fae26802fff63ebcea36da519aa326
w32siooo <creeco@gmail.com>
2019-04-11 10:49:32 +0200

    nu virker det igen.

--- src/com/company/MajorScale.java
+++ src/com/company/MajorScale.java
@@ -3,22 +3,23 @@ package com.company;
 import java.util.Arrays;
 
 public class MajorScale extends ScaleGenerator {
+    private double[] modifiedScale;
 
     public MajorScale(int scaleLength, double startFreq) {
 
         //ScaleLength has to be +4 because there are 4 less tones in a major scale than in a Chromatic scale, and it helps the user logically if we tell them they are creating a scale of just
         super(scaleLength+4, startFreq);
 
-        this.scale = new double[scaleLength-1];
+        this.modifiedScale = new double[scaleLength-1];
 
-        this.scale[0]=scale[0];
-        this.scale[1]=scale[2];
-        this.scale[2]=scale[4];
-        this.scale[3]=scale[5];
-        this.scale[4]=scale[7];
-        this.scale[5]=scale[9];
-        this.scale[6]=scale[11];
-        this.scale[7]=scale[12];
+        this.modifiedScale[0]=scale[0];
+        this.modifiedScale[1]=scale[2];
+        this.modifiedScale[2]=scale[4];
+        this.modifiedScale[3]=scale[5];
+        this.modifiedScale[4]=scale[7];
+        this.modifiedScale[5]=scale[9];
+        this.modifiedScale[6]=scale[11];
+        this.modifiedScale[7]=scale[12];
 
     }
 
