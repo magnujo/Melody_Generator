3fbb66f157273c471e58763281b2a354871872a7
magnujo <48016378+magnujo@users.noreply.github.com>
2019-04-18 17:44:48 +0200

    HashMap virker

---
+++ src/com/company/Hash.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Hash {
+
+
+
+    public static void main(String[] args) {
+
+        ScaleGeneratorTest scaleGenerator = new ScaleGeneratorTest(150, 16.351597831287414);
+        HashMap<String,Double> hashMap = new HashMap<>();
+        String[] noteNames = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B"};
+        System.out.println("hashmap empty?" + hashMap.isEmpty());
+        int j = 0;
+        int octaveNumber = 0;
+        for (int i = 0; i < scaleGenerator.getScale().size(); i++) {
+
+            if (i%12==0 && i!=0) {
+                j = 0;
+                octaveNumber++;
+            }
+            hashMap.put(noteNames[j]+octaveNumber,scaleGenerator.getScale().get(i));
+            j++;
+        }
+
+
+     System.out.println(hashMap.get("F4"));
+
+    }
+
+}
+
+
+
+
+
+
