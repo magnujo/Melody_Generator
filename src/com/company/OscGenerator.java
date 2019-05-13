892104cbb52325719645cc7c738b790fb0d00cdc
w32siooo <creeco@gmail.com>
2019-05-13 10:42:32 +0200

    int octaves removed

--- src/com/company/OscGenerator.java
+++ src/com/company/OscGenerator.java
@@ -32,9 +32,9 @@ public class OscGenerator {
 
     ArrayList<Integer> intRhytmList;
 
-    OscGenerator (int octaves){
+    OscGenerator (){
 
-        FileReader fileReader = new FileReader(".idea/data", octaves);
+        FileReader fileReader = new FileReader(".idea/data");
 
         this.intRhytmList = fileReader.getPlaylist();
 
